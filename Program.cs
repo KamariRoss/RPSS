a056cf958f73e2f6a2621f88a1cde2640b6e3f4a
KamariRoss <kamarimobile@gmail.com>
2020-02-11 16:25:34 -0500

    update1: Added user validation

--- Program.cs
+++ Program.cs
@@ -9,91 +9,105 @@ namespace RPSS
       // As a user, I should see a welcome message.
       Console.WriteLine("Welcome to rock, paper, scissors, Shoot!");
       Console.WriteLine("What is your name?");
+
       var user = Console.ReadLine();
+
       Console.WriteLine(user + " Prepare to be defeated!");
       Console.WriteLine("Round 1");
+
       // As a user, I should see the options I can pick (i.e., "rock", "paper", "scissor")
       Console.WriteLine(user + " Pick rock, paper or scissors?");
       Console.WriteLine("Your answer");
-      var answer = Console.ReadLine();
-      // The computer should randomly decide one of the options.
-      Random rnd = new Random();
-      string[] gameOptions = { "rock", "paper", "scissors" };
-      var gindex = rnd.Next(gameOptions.Length);
-      Console.WriteLine("Your oppenent answered  " + gameOptions[gindex]);
-      // You program should then decide who the winner is;
 
-      var result = "unkown";
+      var answer = Console.ReadLine().ToLower();
 
-      // If the person picks rock and the computer picks rock then record the result of a scratch
-      // If the person picks rock and the computer picks rock then record the result of you lose
-      // ...
-      // ...
-      // ...
-
-      if (answer == "rock")
+      if (answer == "rock" || answer == "paper" || answer == "scissor")
       {
-        // what do we do if the user picked rock
-        // See what the computer picked and compare, see who wins
-        if (gameOptions[gindex] == "rock")
+        // The computer should randomly decide one of the options.
+        Random rnd = new Random();
+        string[] gameOptions = { "rock", "paper", "scissors" };
+        var gindex = rnd.Next(gameOptions.Length);
+        Console.WriteLine("Your oppenent answered  " + gameOptions[gindex]);
+        // You program should then decide who the winner is;
+
+        var result = "unkown";
+
+        // If the person picks rock and the computer picks rock then record the result of a scratch
+        // If the person picks rock and the computer picks rock then record the result of you lose
+        // ...
+        // ...
+        // ...
+
+        if (answer == "rock")
         {
-          result = "It is a draw";
+          // what do we do if the user picked rock
+          // See what the computer picked and compare, see who wins
+          if (gameOptions[gindex] == "rock")
+          {
+            result = "It is a draw";
+          }
+          // scratch
+          if (gameOptions[gindex] == "paper")
+          {
+            result = "You lose";
+          }
+          // pc wins
+          if (gameOptions[gindex] == "scissors")
+          // user wins
+          {
+            result = "You win";
+          }
+
         }
-        // scratch
-        if (gameOptions[gindex] == "paper")
+        if (answer == "paper")
         {
-          result = "You lose";
+          if (gameOptions[gindex] == "paper")
+          // scratch
+          {
+            result = "It is a draw";
+          }
+          if (gameOptions[gindex] == "rock")
+          // user wins
+          {
+            result = "You win";
+          }
+          if (gameOptions[gindex] == "scissors")
+          // pc wins
+          {
+            result = "You lose";
+          }
         }
-        // pc wins
-        if (gameOptions[gindex] == "scissors")
-        // user wins
+
+        if (answer == "scissors")
         {
-          result = "You win";
+          if (gameOptions[gindex] == "paper")
+          // user wins
+          {
+            result = "You lose";
+          }
+          if (gameOptions[gindex] == "rock")
+          // pc wins
+          {
+            result = "You win";
+          }
+          if (gameOptions[gindex] == "scissors")
+          //  scratch
+          {
+            result = "It is a draw";
+          }
+
         }
+
+        // The user should then see the randomly selected option, as well as a win or lose message.
+        Console.WriteLine("Player 2 answered " + gameOptions[gindex] + " you " + result);
+        // add some validation around user input.
 
       }
-      if (answer == "paper")
+      else
       {
-        if (gameOptions[gindex] == "paper")
-        // scratch
-        {
-          result = "It is a draw";
-        }
-        if (gameOptions[gindex] == "rock")
-        // user wins
-        {
-          result = "You win";
-        }
-        if (gameOptions[gindex] == "scissors")
-        // pc wins
-        {
-          result = "You lose";
-        }
+        Console.WriteLine("That isn't one of the choices. Run the game again.");
       }
 
-      if (answer == "scissors")
-      {
-        if (gameOptions[gindex] == "paper")
-        // user wins
-        {
-          result = "You lose";
-        }
-        if (gameOptions[gindex] == "rock")
-        // pc wins
-        {
-          result = "You win";
-        }
-        if (gameOptions[gindex] == "scissors")
-        //  scratch
-        {
-          result = "It is a draw";
-        }
-
-      }
-
-      // The user should then see the randomly selected option, as well as a win or lose message.
-      Console.WriteLine("Player 2 answered " + gameOptions[gindex] + " you " + result);
-      // add some validation around user input.
     }
   }
 }
