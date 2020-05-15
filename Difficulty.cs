a9b1c69833685737d770e635dd8469c087b26ab6
KamariRoss <kamarimobile@gmail.com>
2020-05-15 15:31:27 -0400

    Improved UE for user ansers

--- Difficulty.cs
+++ Difficulty.cs
@@ -3,158 +3,208 @@ namespace RPSS
 {
     class Difficulty
     {
-        public static void DifficultySetting(string userDifficultySelection, string user)
+        public static void DifficultySettings(string playerOne)
         {
-            if (userDifficultySelection != "normal" && userDifficultySelection != "easy" && userDifficultySelection != "impossible")
-            {
-                Console.WriteLine("please try again");
-            }
-            if (userDifficultySelection == "easy")
-            // make every input a victory
-            {
-                Console.WriteLine("You selected easy");
-                Console.WriteLine(user + " Prepare to be defeated!");
-                Console.WriteLine("Round 1");
 
-                // As a user, I should see the options I can pick (i.e., "rock", "paper", "scissor")
-                Console.WriteLine(user + " Pick rock, paper or scissors?");
-                Console.WriteLine("Your answer");
-
-                var answer = Console.ReadLine().ToLower();
-                if (answer == "rock")
-                    Console.WriteLine("PC selected scissors, you win!");
-                if (answer == "paper")
-                    Console.WriteLine("PC selected paper, you win!");
-                if (answer == "scissors")
-                    Console.WriteLine("PC selected paper, you win!");
-                if (answer != "rock" && answer != "paper" && answer != "scissor")
+            bool incorrectAnswer = true;
+            while (incorrectAnswer)
+            {
+                Console.WriteLine(playerOne + " " + "select difficulty: easy, normal, impossible");
+                var difficulty = Console.ReadLine().ToLower();
+                if (difficulty != "normal" && difficulty != "easy" && difficulty != "impossible")
                 {
-                    Console.WriteLine("Please try again");
+                    incorrectAnswer = true;
+                    Console.WriteLine("please try again");
                 }
+                if (difficulty == "easy")
 
-
-            }
-            if (userDifficultySelection == "normal")
-            {
-
-                Console.WriteLine(user + " Prepare to be defeated!");
-                Console.WriteLine("Round 1");
-
-                // As a user, I should see the options I can pick (i.e., "rock", "paper", "scissor")
-                Console.WriteLine(user + " Pick rock, paper or scissors?");
-                Console.WriteLine("Your answer");
-
-                var answer = Console.ReadLine().ToLower();
-
-                if (answer == "rock" || answer == "paper" || answer == "scissor")
+                // make every input a victory
                 {
-                    // The computer should randomly decide one of the options.
-                    Random rnd = new Random();
-                    string[] gameOptions = { "rock", "paper", "scissors" };
-                    var gindex = rnd.Next(gameOptions.Length);
-                    Console.WriteLine("Your oppenent answered  " + gameOptions[gindex]);
-                    // You program should then decide who the winner is;
-
-                    var result = "unkown";
-
-                    // If the person picks rock and the computer picks rock then record the result of a scratch
-                    // If the person picks rock and the computer picks rock then record the result of you lose
-                    // ...
-                    // ...
-                    // ...
-
-                    if (answer == "rock")
+                    incorrectAnswer = false;
+                    Console.WriteLine("You selected easy");
+                    Console.WriteLine(playerOne + " Prepare to be defeated!");
+                    bool easyErrorCheck = true;
+                    while (easyErrorCheck)
                     {
-                        // what do we do if the user picked rock
-                        // See what the computer picked and compare, see who wins
-                        if (gameOptions[gindex] == "rock")
+                        // As a playerOne, I should see the options I can pick (i.e., "rock", "paper", "scissor")
+                        Console.WriteLine(playerOne + " Pick rock, paper or scissors?");
+                        Console.WriteLine("Your answer");
+
+                        var answer = Console.ReadLine().ToLower();
+                        if (answer == "rock" || answer == "paper" || answer == "scissor")
                         {
-                            result = "it is a draw";
+                            easyErrorCheck = false;
+
+                            if (answer == "rock")
+                            {
+                                Console.WriteLine("PC selected scissors, you win!");
+                            }
+                            if (answer == "paper")
+                            {
+                                Console.WriteLine("PC selected paper, you win!");
+                            }
+                            if (answer == "scissors")
+                            {
+                                Console.WriteLine("PC selected paper, you win!");
+                            }
                         }
-                        // scratch
-                        if (gameOptions[gindex] == "paper")
+                        if (answer != "rock" && answer != "paper" && answer != "scissor")
                         {
-                            result = "you lose";
-                        }
-                        // pc wins
-                        if (gameOptions[gindex] == "scissors")
-                        // user wins
-                        {
-                            result = "you win";
+                            Console.WriteLine("Please try again");
+                            easyErrorCheck = true;
                         }
 
                     }
-                    if (answer == "paper")
+                }
+                if (difficulty == "normal")
+                {
+
+                    incorrectAnswer = false;
+                    Console.WriteLine(playerOne + " Prepare to be defeated!");
+                    bool normalErrorCheck = true;
+                    while (normalErrorCheck)
                     {
-                        if (gameOptions[gindex] == "paper")
-                        // scratch
+                        // As a playerOne, I should see the options I can pick (i.e., "rock", "paper", "scissor")
+                        Console.WriteLine(playerOne + " Pick rock, paper or scissors?");
+                        Console.WriteLine("Your answer");
+
+                        var answer = Console.ReadLine().ToLower();
+                        if (answer != "rock" && answer != "paper" && answer != "scissor")
                         {
-                            result = "it is a draw";
+                            normalErrorCheck = true;
+                            Console.WriteLine("Invalid answer, please try again");
                         }
-                        if (gameOptions[gindex] == "rock")
-                        // user wins
+                        if (answer == "rock" || answer == "paper" || answer == "scissor")
                         {
-                            result = "you win";
+                            normalErrorCheck = false;
+                            // The computer should randomly decide one of the options.
+                            Random rnd = new Random();
+                            string[] gameOptions = { "rock", "paper", "scissors" };
+                            var gindex = rnd.Next(gameOptions.Length);
+                            Console.WriteLine("Your oppenent answered  " + gameOptions[gindex]);
+                            // You program should then decide who the winner is;
+
+                            var result = "unkown";
+
+                            // If the person picks rock and the computer picks rock then record the result of a scratch
+                            // If the person picks rock and the computer picks rock then record the result of you lose
+                            // ...
+                            // ...
+                            // ...
+
+                            if (answer == "rock")
+                            {
+                                // what do we do if the playerOne picked rock
+                                // See what the computer picked and compare, see who wins
+                                if (gameOptions[gindex] == "rock")
+                                {
+                                    result = "it is a draw";
+                                }
+                                // scratch
+                                if (gameOptions[gindex] == "paper")
+                                {
+                                    result = "you lose";
+                                }
+                                // pc wins
+                                if (gameOptions[gindex] == "scissors")
+                                // playerOne wins
+                                {
+                                    result = "you win";
+                                }
+
+
+                            }
+                            if (answer == "paper")
+                            {
+                                if (gameOptions[gindex] == "paper")
+                                // scratch
+                                {
+                                    result = "it is a draw";
+                                }
+                                if (gameOptions[gindex] == "rock")
+                                // playerOne wins
+                                {
+                                    result = "you win";
+                                }
+                                if (gameOptions[gindex] == "scissors")
+                                // pc wins
+                                {
+                                    result = "you lose";
+                                }
+
+                            }
+
+                            if (answer == "scissors")
+                            {
+                                if (gameOptions[gindex] == "paper")
+                                // playerOne wins
+                                {
+                                    result = "you lose";
+                                }
+                                if (gameOptions[gindex] == "rock")
+                                // pc wins
+                                {
+                                    result = "you win";
+                                }
+                                if (gameOptions[gindex] == "scissors")
+                                //  scratch
+                                {
+                                    result = "it is a draw";
+                                }
+
+                            }
+                            // The playerOne should then see the randomly selected option, as well as a win or lose message.
+                            Console.WriteLine("Player 2 answered " + gameOptions[gindex] + " " + result);
+                            // add some validation around playerOne input.
+
                         }
-                        if (gameOptions[gindex] == "scissors")
-                        // pc wins
+                        if (answer != "rock" && answer != "paper" && answer != "scissor")
                         {
-                            result = "you lose";
+                            Console.WriteLine("Please try again");
+
                         }
                     }
+                }
+                if (difficulty == "impossible")
 
-                    if (answer == "scissors")
+                {
+                    incorrectAnswer = false;
+                    Console.WriteLine("You selected impossible");
+                    Console.WriteLine(playerOne + " " + "Prepare to be defeated!");
+                    bool impossibleErrorCheck = true;
+                    while (impossibleErrorCheck)
                     {
-                        if (gameOptions[gindex] == "paper")
-                        // user wins
-                        {
-                            result = "you lose";
-                        }
-                        if (gameOptions[gindex] == "rock")
-                        // pc wins
-                        {
-                            result = "you win";
-                        }
-                        if (gameOptions[gindex] == "scissors")
-                        //  scratch
-                        {
-                            result = "it is a draw";
-                        }
+                        Console.WriteLine(playerOne + " " + "Pick rock, paper or scissors");
+                        Console.WriteLine("Your answer");
 
+                        var answer = Console.ReadLine().ToLower();
+                        if (answer == "rock" || answer == "paper" || answer == "scissor")
+                        {
+                            impossibleErrorCheck = false;
+
+                            if (answer == "rock")
+                            {
+                                Console.WriteLine("PC selected scissors you lose");
+                            }
+                            if (answer == "paper")
+                            {
+                                Console.WriteLine("PC selected scissors, you lose");
+                            }
+                            if (answer == "scissors")
+                            {
+                                Console.WriteLine("PC selected rock, you lose");
+                            }
+                        }
+                        if (answer != "rock" && answer != "paper" && answer != "scissor")
+                        {
+                            Console.WriteLine("Please try again");
+
+                            impossibleErrorCheck = true;
+                        }
                     }
-                    // The user should then see the randomly selected option, as well as a win or lose message.
-                    Console.WriteLine("Player 2 answered " + gameOptions[gindex] + " " + result);
-                    // add some validation around user input.
-
-
                 }
-                if (answer != "rock" && answer != "paper" && answer != "scissor")
-                {
-                    Console.WriteLine("Please try again");
-                }
-
             }
-            if (userDifficultySelection == "impossible")
-            {
-                Console.WriteLine("You selected impossible");
-                Console.WriteLine(user + "Prepare to be defeated!");
-                Console.WriteLine("Round 1");
-                Console.WriteLine(user + "Pick rock, paper or scissors");
-                Console.WriteLine("Your answer");
 
-                var answer = Console.ReadLine().ToLower();
-                if (answer == "rock")
-                    Console.WriteLine("PC selected scissors you lose");
-                if (answer == "paper")
-                    Console.WriteLine("PC selected scissors, you lose");
-                if (answer == "scissors")
-                    Console.WriteLine("PC selected rock, you lose");
-                if (answer != "rock" && answer != "paper" && answer != "scissor")
-                {
-                    Console.WriteLine("Please try again");
-                }
-
-            }
         }
 
 
--- Program.cs
+++ Program.cs
@@ -7,27 +7,40 @@ namespace RPSS
         static void Main(string[] args)
 
         {
-
-
-
             // As a user, I should see a welcome message.
             Console.WriteLine("Welcome to rock, paper, scissors, Shoot!");
             Console.WriteLine("What is your name?");
 
             var user = Console.ReadLine();
-            bool flag = true;
-            while (flag)
+            bool repeatGame = true;
+            while (repeatGame)
             {
-                // User selecting difficulty
-                Console.WriteLine(user + "select difficulty: easy, normal, impossible");
-                var difficulty = Console.ReadLine().ToLower();
-                Difficulty.DifficultySetting(difficulty, user);
-                Console.WriteLine("Play again? 'y' or 'n'");
-                string playAgain = null;
-                playAgain = Console.ReadLine().ToLower();
-                if (playAgain == "n")
+                Difficulty.DifficultySettings(user);
+
+                bool playAgainErrorCheck = true;
+                while (playAgainErrorCheck)
                 {
-                    flag = false;
+                    Console.WriteLine("Play again? 'yes' or 'no'");
+                    // string playAgain = null;
+                    string playAgain = Console.ReadLine().ToLower();
+                    if (playAgain == "yes" || playAgain == "no")
+                    {
+                        playAgainErrorCheck = false;
+                        if (playAgain == "no")
+                        {
+                            repeatGame = false;
+                        }
+                        if (playAgain == "yes")
+                        {
+                            repeatGame = true;
+                        }
+                    }
+                    if (playAgain != "no" && playAgain != "yes")
+                    {
+                        Console.WriteLine("Please try again, wrong answer");
+                        playAgainErrorCheck = true;
+
+                    }
                 }
             }
         }
